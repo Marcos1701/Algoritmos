d343ed291dd2d31bb12a2320ccbb00477fe4ad17
Marcos1701 <103659504+Marcos1701@users.noreply.github.com>
2022-06-06 16:51:07 -0300

    Update bee_1168.js

--- bee_strings/bee_1168.js
+++ bee_strings/bee_1168.js
@@ -3,60 +3,56 @@ const input = fs.readFileSync('stdin', 'utf-8')
 const entradas = input.split('\n')
 
 
-function main(){
+function main() {
     const qtd_leitura = Number(entradas[0])
     calcular_num_leds(qtd_leitura)
 
 }
 
-function calcular_num_leds(limite_leitura){
+function calcular_num_leds(limite_leitura) {
 
-    let index = 1
-    let valor_leitura = entradas[index]
+    let valor_leitura = entradas[1]
 
-    while(index <= limite_leitura){
+    for (let i = 2; i <= limite_leitura + 1; i++) {
         const num_leds = conferir_leds_necessarios(valor_leitura)
         console.log(`${num_leds} leds`)
-        index++
-        valor_leitura = entradas[index]
+        valor_leitura = entradas[i]
     }
 
 }
 
-function conferir_leds_necessarios(valor){
+function conferir_leds_necessarios(valor) {
     //let indice = 0
+    const valor_frag = valor.split('')
     let num_leds = 0
 
-    for(let i in valor){
-    num_leds += pegar_num_leds_para_det_num(valor[i])
-     
+    for (let num of valor_frag) {
+        num_leds += pegar_num_leds_para_det_num(num)
     }
     return num_leds
 }
 
-function pegar_num_leds_para_det_num(valor_recebido){
+function pegar_num_leds_para_det_num(valor_recebido) {
     const valor = Number(valor_recebido)
 
     let valor_retorno = 0
 
-    if(valor === 0 || valor === 9 || valor === 6){
+    if (valor == 0 || valor == 9 || valor == 6) {
         valor_retorno = 6
         return valor_retorno
-    }else if(valor === 5 || valor === 3 || valor === 2){
+    } else if (valor == 5 || valor == 3 || valor == 2) {
         valor_retorno = 5
         return valor_retorno
-    }else if(valor === 8){
+    } else if (valor == 8) {
         valor_retorno = 7
         return valor_retorno
-
-    }else if(valor === 7){
+    } else if (valor == 7) {
         valor_retorno = 3
         return valor_retorno
-    }else if(valor === 4){
+    } else if (valor == 4) {
         valor_retorno = 4
         return valor_retorno
-    
-    }else if(valor === 1){
+    } else {
         valor_retorno = 2
         return valor_retorno
     }
